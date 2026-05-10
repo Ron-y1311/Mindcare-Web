8eed9381262baea62cc1435700ca595dbdbfd6b1
Rony Campos <camposparedesrony1311@gmail.com>
2026-05-10 15:20:00 -0500

    Agregar función obtenerHistorialEmocional en las interfaces del Backend

--- MindCare-Backend/src/main/java/com/upc/mindcare/interfaces/ITrackingService.java
+++ MindCare-Backend/src/main/java/com/upc/mindcare/interfaces/ITrackingService.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface ITrackingService {
     TrackingDTO registrarEstadoEmocional(TrackingDTO dto);
+   //Se agrega en interfaces la funcion de consultar historial emocional para usarlo en los services
     List<TrackingDTO> consultarHistorialEmocional(Long pacienteId);
     TrackingDTO registrarReflexionDiaria(Long trackingId, TrackingDTO dto);
     TrackingDTO obtenerUltimoTracking(Long pacienteId);
