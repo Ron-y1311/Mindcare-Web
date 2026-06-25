1eaa7d74395858fb20f55062fefa60a9b292eb4a
Diego Baldeon <diegobaldeonhuaman@gmail.com>
2026-06-25 17:15:01 -0500

    Configurar rutas protegidas en el frontend

--- MindCare-Frontend/src/app/guards/role.guard.ts
+++ MindCare-Frontend/src/app/guards/role.guard.ts
@@ -1,3 +1,4 @@
+//Implementar rutas protegidas en frontend
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth-service';
